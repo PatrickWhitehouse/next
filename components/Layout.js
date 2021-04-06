be91848876edd3c24f42e4edf756ec6ea7a07e8a
Patrick <patrick.whitehouse@pushon.co.uk>
2021-04-06 21:45:57 +0100

    404

--- components/Layout.js
+++ components/Layout.js
@@ -3,11 +3,11 @@ import Footer from "./Footer";
 
 const Layout = ({children}) => {
     return(
-        <>
+        <div className="bg-gray-100 h-screen">
             <Navbar/>
             {children}
             <Footer/>
-        </>
+        </div>
     )
 }
 
---
+++ pages/404.js
@@ -0,0 +1,7 @@
+const Custom404 = () => {
+    return(
+        <h1>Oops. This page doesn't exist.</h1>
+    )
+}
+
+export default Custom404;
