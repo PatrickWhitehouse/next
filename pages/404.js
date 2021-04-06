162b1126194df5c85c0e3e2d9973e158a5069e45
Patrick <patrick.whitehouse@pushon.co.uk>
2021-04-06 22:14:21 +0100

    add redirect

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,31 @@
+import {useRouter} from "next/router";
+import {useEffect, useState} from 'react';
+
 const Custom404 = () => {
+
+    const router = useRouter();
+    const [sec, setSec] = useState(3);
+
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/');
+        }, 3000)
+    }, [])
+
+    useEffect(() => {
+        if(sec !== 1) {
+            setTimeout(() => {
+                setSec(sec => sec - 1);
+            }, 1000)
+        }
+    }, [sec]);
+
+
     return(
-        <h1>Oops. This page doesn't exist.</h1>
+        <>
+            <h1>Oops. This page doesn't exist.</h1>
+            <h2>You will be redirected back to the homepage in {sec} seconds.</h2>
+        </>
     )
 }
 
