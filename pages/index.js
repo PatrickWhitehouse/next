ee8c7b98361e1b1a939cb7cced5aa0847e2bf307
Patrick <patrick.whitehouse@pushon.co.uk>
2021-04-06 21:41:12 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
-
-
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
           <h1>Polished Pads</h1>
           <h2>Automotive enhancement specialists</h2>
     </div>
